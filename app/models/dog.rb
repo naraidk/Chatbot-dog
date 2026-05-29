814d6e82466e76e34a9f76a4121f4760d4d89287
Naraid Kuapunyakoon <naraid.kuapunyakoon@gmail.com>
2026-05-29 11:11:59 +0200

    Image profile ajouté

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog < ApplicationRecord
   belongs_to :user
 
   has_many :chats, dependent: :destroy
-
+  has_one_attached :photo # [photo upload](ca://s?q=Add_photo_to_Dog_model)
   validates :name, presence: true
   validates :breed, presence: true
 end
